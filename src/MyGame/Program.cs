1f5a8a019833882582c9c6b01a7b6f0289ff135e
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-03-23 16:42:10 -0400

    Initial commit

---
+++ src/MyGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using Vigilance.Core;
+using Vigilance.Drawing;
+using Vigilance.Input;
+
+var config = new GameConfig
+{
+    Title = "My Game",
+    ExitKey = Key.Escape,
+    FullscreenKey = Key.Tab,
+};
+
+var scene = new Scene();
+
+scene.OnUpdate(() =>
+{
+    Renderer.Graphics.ClearBackground(Color.Blue);
+});
+
+Game.Launch(config, scene);
